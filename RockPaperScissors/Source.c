255d0e809b07029c3a118b60ce8f17295c21494f
flavii porumbiel <flaviusporumbiel@outlook.com>
2021-11-08 12:29:32 -0500

    added all the if statements in the main application as well as the test unit in which player2 wins as scissors - currently it fails. will have to fix it

--- RockPaperScissors/Source.c
+++ RockPaperScissors/Source.c
@@ -13,12 +13,53 @@ int RockPaperScissors(char player1[], char player2[])
 	if ((player1 == "Paper") > 0 || (player2 == "Paper") > 0 || (player1 == "Rock") > 0 || (player2 == "Rock") > 0 || (player1 == "Scissors") > 0 || (player2 == "Scissors") > 0)
 	{
 		printf("Not Valid!");
-		return 30;
+		return 3;
 	}
 
 	if (player1 == player2)
 	{
 		printf("Draw!");
 		return 0;
+	}
+
+	if (player1 == "Paper")
+	{
+		if (player2 == "Scissors")
+		{
+			printf("Player2");
+			return 2;
+		}
+		if (player2 == "Rock");
+		{
+			printf("Player1");
+			return 1;
+		}
+
+	if (player1 == "Rock")
+	{
+		if (player2 == "Paper")
+		{
+			printf("Player2");
+			return 2;
+		}
+		if (player2 == "Scissors");
+		{
+			printf("Player1");
+			return 1;
+		}
+
+		if (player1 == "Scissors")
+		{
+			if (player2 == "Rock")
+			{
+				printf("Player2");
+				return 2;
+			}
+			if (player2 == "Paper");
+			{
+				printf("Player1");
+				return 1;
+			}
+		}
 	}
 }
--- RockPaperScissorsTest/RockPaperScissorsTest.cpp
+++ RockPaperScissorsTest/RockPaperScissorsTest.cpp
@@ -22,5 +22,14 @@ namespace RockPaperScissorsTest
 			int gameResult = RockPaperScissors(a, b);
 			Assert::AreEqual(0, gameResult);
 		}
+
+		TEST_METHOD(T1_gameResult_P1_Paper_P2_Scissors_P2win)
+		{
+			char a[6] = { 'P','a','p','e','r' };
+			char b[9] = { 'S','c','i','s','s','o','r','s' };
+
+			int gameResult = RockPaperScissors(a, b);
+			Assert::AreEqual(2, gameResult);
+		}	
 	};
 }
